c12be2f372612610ffd7a995aa2d5259709daa6d
vekonymartin <vekonymartin@stud.uni-obuda.hu>
2019-09-25 15:10:36 +0200

    Finish _OuterVariableTrap project

--- 06_OuterVariableTraps/Program.cs
+++ 06_OuterVariableTraps/Program.cs
@@ -10,6 +10,21 @@ namespace _06_OuterVariableTraps
     {
         static void Main(string[] args)
         {
+            // we have several SDXC cards ...1GB, 2GB, 4GB
+            // task: calculate how much  cards have to buy to store the entire data?
+
+            Action<int> calculatros = null;
+
+            for (int i = 1; i < 4096; i *= 2)
+            {
+                int k = i;
+                calculatros += (dataSize) =>
+                {
+                    Console.WriteLine($"{k} GB Card: {(dataSize / k) + 1} PIECE");
+                };
+            }
+
+            calculatros(1564); // GB
         }
     }
 }
