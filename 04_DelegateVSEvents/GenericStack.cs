8ab2da8ea39d3779cc26f336d82caaf89fd79086
vekonymartin <vekonymartin@stud.uni-obuda.hu>
2019-09-25 09:27:55 +0200

    Finish DelegatesVSEvents projects

---
+++ 04_DelegateVSEvents/GenericStack.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_DelegateVSEvents
+{
+    class PlacedEventArgs<T> : EventArgs
+    {
+        public T Value { get; set; }
+        public int Location { get; set; }
+    }
+    class GenericStack<T> 
+    {
+        //public delegate void EventType(T value, int location); --> If we use own delegate
+        public event EventHandler<PlacedEventArgs<T>> numberPlaced;
+
+        T[] array;
+        int pointer;
+
+        public GenericStack(int size)
+        {
+            array = new T[size];
+            pointer = 0;
+        }
+
+        public void Push(T item)
+        {
+            if (pointer > array.Length)
+            {
+                throw new ArgumentException("StackOverFlow!");
+            }
+            array[pointer++] = item;
+
+            // Event is here
+            numberPlaced?.Invoke(this, new PlacedEventArgs<T>
+            {
+                Value = item,
+                Location = pointer - 1
+            });
+        }
+
+        public T Pop()
+        {
+            if (pointer == 0)
+            {
+                throw new ArgumentException("Stack is Empty!");
+            }
+            return array[--pointer];
+        }
+
+        public string ToString(string item)
+        {
+            return item;
+        }
+    }
+}
--- 04_DelegateVSEvents/Program.cs
+++ 04_DelegateVSEvents/Program.cs
@@ -8,8 +8,31 @@ namespace _04_DelegateVSEvents
 {
     class Program
     {
+        private static void Display(int value, int location)
+        {
+            Console.WriteLine($"{value} placed at {location}");
+        }
+
+        private static void NumberStack_NumberPlaced(object sender, PlacedEventArgs<int> e)
+        {
+            Console.WriteLine($"{e.Value} placed at {e.Location}");
+        }
         static void Main(string[] args)
         {
+            // LIFO data structure
+            GenericStack<int> numberStack = new GenericStack<int>(10);
+
+            numberStack.numberPlaced += NumberStack_NumberPlaced;
+
+            numberStack.Push(14);
+            numberStack.Push(1);
+            numberStack.Push(6);
+            numberStack.Push(15);
+            numberStack.Push(22);
+            numberStack.Push(33);
+
+            Console.WriteLine(numberStack.Pop());
+
         }
     }
 }
