2bb45b9d4c71f8c132192c0a457202d949573e10
vekonymartin <vekonymartin@stud.uni-obuda.hu>
2019-09-25 09:46:43 +0200

    Finish AnonMeth_Lambda project

---
+++ 05_AnonMeh_Lambda/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_AnonMeh_Lambda
+{
+    class Person
+    {
+        static Random r;
+        static string[] names;
+
+        public string Name  { get; set; }
+        public int Age      { get; set; }
+
+        static Person()
+        {
+            r = new Random();
+            string[] tmpnames = { "John", "Kate", "Alfred", "William", "Brian", "Stuart", "Jessica", "Laurel", "Oliver" };
+            names = tmpnames;
+        }
+
+        public Person()
+        {
+            this.Name = names[r.Next(0, names.Length)];
+            this.Age = r.Next(18, 90);
+        }
+    }
+}
--- 05_AnonMeh_Lambda/Program.cs
+++ 05_AnonMeh_Lambda/Program.cs
@@ -8,8 +8,33 @@ namespace _05_AnonMeh_Lambda
 {
     class Program
     {
+        private static bool OlderThan30(Person p)
+        {
+            return p.Age > 30;
+        }
+
+        private static int SpecialComparer(Person p1, Person p2)
+        {
+            return p1.Name[0].CompareTo(p2.Name[0]);
+        }
         static void Main(string[] args)
         {
+            List<Person> friends = new List<Person>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                friends.Add(new Person());
+            }
+
+            // Filtering: older than 30
+            // 01. Use named method matching Predicate<T>
+            List<Person> sol2 = friends.FindAll(delegate (Person p)
+            {
+                return p.Age > 30;
+            });
+
+            // Use lambda expression mathcing Predicate<T>
+            List<Person> sol3 = friends.FindAll(p => p.Age > 30);
         }
     }
 }
